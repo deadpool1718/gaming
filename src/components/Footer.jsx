4f3469762e53b5ad64802e8d1e333e4ce25dd92f
Amit Sahani <95041447+deadpool1718@users.noreply.github.com>
2025-11-14 23:35:23 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import "boxicons/css/boxicons.min.css";
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-between lg:mt-[15%] mt-[25%] py-8 lg:px-32 md:px-16 px-8 border-t-[0.3px] border-[#babaff]">
+  <img className="h-10" src={`${import.meta.env.BASE_URL}Images/illu-text.png`} alt="illu-text" />
+  <img className="h-16 md:inline hidden" src={`${import.meta.env.BASE_URL}Images/illu-logo.png`} alt="illu-logo" />
+      <div className="flex gap-4">
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+          href="#"
+        >
+          {" "}
+          <i className="bx bxl-twitter"></i>{" "}
+        </a>
+
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+          href="#"
+        >
+          {" "}
+          <i className="bx bxl-youtube"></i>{" "}
+        </a>
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+          href="#"
+        >
+          {" "}
+          <i className="bx bxl-discord"></i>{" "}
+        </a>
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+          href="#"
+        >
+          {" "}
+          <i className="bx bxl-linkedin"></i>{" "}
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
